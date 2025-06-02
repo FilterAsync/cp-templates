1ed3b790c3d9c2d23641295d14be6e85be5dbac7
goppamach <81472214+FilterAsync@users.noreply.github.com>
2025-06-02 20:38:45 +0700

    added bitwise operations (using uint64_t)

---
+++ misc/bitwise.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<class T> bool chmax(T &a, T const &b) {
+	return (a < b ? (a = b, true) : false);
+}
+
+template<class T> bool chmin(T &a, T const &b) {
+	return (a > b ? (a = b, true) : false);
+}
+
+using ll = long long;
+
+#define el "\n"
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define FOR(i, l, r) for (ll i = l; i <= r; i++)
+#define FOD(i, l, r) for (ll i = l; i >= r; i--)
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+#define LOCAL
+
+const int MAXN = 1E5 + 5;
+const ll MOD = 1E9 + 7, INF = 1E18;
+const double EPS = 1E-9;
+
+int get(uint64_t x, int i) {
+	return (x >> i) & 1;
+}
+
+void set(uint64_t &x, int i) {
+	x |= 1ULL << i;
+}
+
+void clear(uint64_t &x, int i) {
+	x &= ~1ULL << i;
+}
+
+void toggle(uint64_t &x, int i) {
+	x ^= 1ULL << i;
+}
+
+void solve() {
+	
+}
+
+int main() {
+	fast_io
+	#ifndef LOCAL
+		freopen("INPUT.INP", "r", stdin);
+		freopen("OUTPUT.OUT", "w", stdout);
+	#endif
+
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+}
