429b8787efc579cd10615d33566f9ccaadc71583
goppamach <81472214+FilterAsync@users.noreply.github.com>
2025-06-10 17:17:14 +0700

    added grading mechanism

---
+++ misc/grading/example/2SUM.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<class T> bool chmax(T &a, T const &b) {
+    return (a < b ? (a = b, true) : false);
+}
+
+template<class T> bool chmin(T &a, T const &b) {
+    return (a > b ? (a = b, true) : false);
+}
+
+using ll = long long;
+
+#define el "\n"
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define FOR(i, l, r) for (ll i = l; i <= r; i++)
+#define FOD(i, l, r) for (ll i = l; i >= r; i--)
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define by(x) [](auto const &a, auto const &b) { return a.x < b.x; }
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+// #define LOCAL
+
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+const string NAME = "2SUM";
+const int MAXN = 1E5 + 5;
+const int INF = 1E9 + 7;
+const ll MOD = 1E9 + 7, INFLL = 1E18;
+const double EPS = 1E-9;
+
+void solve() {
+	// optimal solution for the two-sum problem to compare against a brute
+	// force solution
+    int n, t;
+	cin >> n >> t;
+
+	int a[n + 1];
+	FOR(i, 1, n) {
+		cin >> a[i];
+	}
+
+	unordered_map<int, int> mp;
+	FOR(i, 1, n) {
+		int complement = t - a[i];
+		if (mp.find(complement) != mp.end()) {
+			cout << "YES" << el;
+			return;
+		}
+		mp[a[i]] = i;
+	}
+
+	cout << "NO" << el;
+}
+
+int main() {
+    fast_io
+    #ifndef LOCAL
+        freopen((NAME + ".INP").c_str(), "r", stdin);
+        freopen((NAME + ".OUT").c_str(), "w", stdout);
+    #endif
+
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
---
+++ misc/grading/example/grading.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<class T> bool chmax(T &a, T const &b) {
+    return (a < b ? (a = b, true) : false);
+}
+
+template<class T> bool chmin(T &a, T const &b) {
+    return (a > b ? (a = b, true) : false);
+}
+
+using ll = long long;
+
+#define el "\n"
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define FOR(i, l, r) for (ll i = l; i <= r; i++)
+#define FOD(i, l, r) for (ll i = l; i >= r; i--)
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define by(x) [](auto const &a, auto const &b) { return a.x < b.x; }
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+#define LOCAL
+
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+const int MAXN = 1E5 + 5;
+const int INF = 1E9 + 7;
+const ll MOD = 1E9 + 7, INFLL = 1E18;
+const double EPS = 1E-9;
+const string NAME = "2SUM"; // program name
+const int NTEST = 100; // number of tests
+
+// find random number in [l, r]
+ll Rand(ll l, ll r) {
+    assert(l <= r);
+    return l + rng() % (r - l + 1);
+}
+
+int main() {
+    srand(time(NULL));
+    FOR(_tc, 1, NTEST) {
+        ofstream inp((NAME + ".INP").c_str());
+		int n = Rand(100, 200);
+		int target = Rand(1, 2025);
+		inp << n << " " << target << el;
+		FOR(i, 1, n) {
+			int x = Rand(1, 1000);
+			inp << x << " ";
+		}
+        inp.close();
+
+        system((NAME + ".exe").c_str());
+        system((NAME + "_sol.exe").c_str());
+
+        if (system(("fc " + NAME + ".OUT " + NAME + ".ANS").c_str()) != 0) {
+            cout << "Test " << _tc << ": WA" << el;
+			return 0;
+        }
+
+    	cout << "Test " << _tc << ": AC" << el;
+    }
+}
