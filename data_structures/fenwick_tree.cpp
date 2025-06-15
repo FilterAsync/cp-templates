c923a6b136a240fa442e0af9a5616b626680162e
goppamach <81472214+FilterAsync@users.noreply.github.com>
2025-06-15 18:24:00 +0700

    minor changes

--- data_structures/fenwick_tree.cpp
+++ data_structures/fenwick_tree.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 template<class T> bool chmax(T &a, T const &b) {
-	return (a < b ? (a = b, true) : false);
+    return (a < b ? (a = b, true) : false);
 }
 
 template<class T> bool chmin(T &a, T const &b) {
-	return (a > b ? (a = b, true) : false);
+    return (a > b ? (a = b, true) : false);
 }
 
 using ll = long long;
@@ -32,46 +32,64 @@ const int MAXN = 1E5 + 5;
 const ll MOD = 1E9 + 7, INF = 1E18;
 const double EPS = 1E-9;
 
-template<typename T> class BIT {
-private:
-	int n;
-	vector<T> bit;
-public:
-	BIT(int n) : n(n) {
-		bit.assign(n + 1, 0);
-	}
+struct BIT {
+    int n;
+    vector<ll> bit;
 
-	void update(int i, T val) {
-		for (; i <= n; i += i & -i) {
-			bit[i] += val;
-		}
-	}
+    BIT(int n) : n(n) {
+        bit.assign(n + 1, 0);
+    }
 
-	T query(int i) {
-		T res = 0;
-		for (; i >= 1; i -= i & -i) {
-			res += bit[i];
-		}
-		return res;
-	}
+    void update(int i, ll val) {
+        for (; i <= n; i += i & -i) {
+            bit[i] += val;
+        }
+    }
 
-	T query(int l, int r) {
-		return query(r) - query(l - 1);
-	}
+    ll query(int i) {
+        ll res = 0;
+        for (; i >= 1; i -= i & -i) {
+            res += bit[i];
+        }
+        return res;
+    }
+
+    ll query(int l, int r) {
+        return query(r) - query(l - 1);
+    }
+
+    int lower_bound(ll val) {
+        int idx = 0;
+        ll sum = 0;
+        for (int i = 1 << 20; i > 0; i >>= 1) {
+            if (idx + i <= n && sum + bit[idx + i] < val) {
+                idx += i;
+                sum += bit[idx];
+            }
+        }
+        return idx + 1;
+    }
 };
 
 void solve() {
-	
+    BIT bit(10);
+    bit.update(1, 5);
+    bit.update(2, 3);
+    bit.update(3, 2);
+    ll val = 9;
+    int idx = bit.lower_bound(val);
+    cout << "The first index with cumulative sum >= " << val << " is: " << idx << el;
+    cout << "Cumulative sum at idx: " << bit.query(idx) << el;
 }
 
 int main() {
-	fast_io
-	#ifndef LOCAL
-		freopen("INPUT.INP", "r", stdin);
-		freopen("OUTPUT.OUT", "w", stdout);
-	#endif
+    fast_io
+    #ifndef LOCAL
+        freopen("INPUT.INP", "r", stdin);
+        freopen("OUTPUT.OUT", "w", stdout);
+    #endif
 
-	int t = 1;
-	// cin >> t;
-	while (t--) solve();
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
 }
--- data_structures/lazy_segment_tree.cpp
+++ data_structures/lazy_segment_tree.cpp
@@ -3,59 +3,64 @@
 using namespace std;
 
 template<class T> bool chmax(T &a, T const &b) {
-    return (a < b ? (a = b, true) : false);
+	return (a < b ? (a = b, true) : false);
 }
 
 template<class T> bool chmin(T &a, T const &b) {
-    return (a > b ? (a = b, true) : false);
+	return (a > b ? (a = b, true) : false);
 }
+
+using ll = long long;
 
 #define el "\n"
 #define pii pair<int, int>
 #define pll pair<ll, ll>
 #define fi first
 #define se second
-#define FOR(i, l, r) for (int i = l; i <= r; i++)
-#define FOD(i, l, r) for (int i = l; i >= r; i--)
+#define FOR(i, l, r) for (ll i = l; i <= r; i++)
+#define FOD(i, l, r) for (ll i = l; i >= r; i--)
+
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) (int)(x).size()
-#define by(x) [](auto const &a, auto const &b) { return a.x < b.x; }
+
 #define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
-using ll = long long;
-using vi = vector<int>;
-using vll = vector<ll>;
-using vpii = vector<pii>;
-
 #define LOCAL
-#ifdef LOCAL
-template<typename T> void _print(const char* name, T&& x) {
-    cerr << name << " = " << x << el;
-}
-#define debug(x) _print(#x, x)
-#else
-#define debug(x)
-#endif
 
-mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
-
-const string NAME = "PROBLEM";
 const int MAXN = 1E5 + 5;
-const int INF = 1E9 + 7;
-const ll MOD = 1E9 + 7, INFLL = 1E18;
+const ll MOD = 1E9 + 7, INF = 1E18;
 const double EPS = 1E-9;
 
+// segment tree with lazy propagation
+// assuming range sum query
 struct SegmentTree {
     int n;
-    vector<ll> tree;
+    vector<ll> tree, lazy;
+
     SegmentTree(int n) : n(n) {
         tree.resize(4 * n + 5);
+        lazy.resize(4 * n + 5);
+    }
+
+    void push(int i, int l, int r) {
+        if (!lazy[i]) return;
+        tree[i] += lazy[i] * (r - l + 1);
+        if (l != r) {
+            lazy[i * 2] += lazy[i];
+            lazy[i * 2 + 1] += lazy[i];
+        }
+        lazy[i] = 0;
     }
 
     void update(int i, int l, int r, int ql, int qr, ll val) {
+        push(i, l, r);
         if (l > r || l > qr || r < ql) return;
-        if (l >= ql && r <= qr) return;
+        if (l >= ql && r <= qr) {
+            lazy[i] += val;
+            push(i, l, r);
+            return;
+        }
         int m = (l + r) / 2;
         update(i * 2, l, m, ql, qr, val);
         update(i * 2 + 1, m + 1, r, ql, qr, val);
@@ -63,6 +68,7 @@ struct SegmentTree {
     }
 
     ll get(int i, int l, int r, int ql, int qr) {
+        push(i, l, r);
         if (l > r || l > qr || r < ql) return 0;
         if (l >= ql && r <= qr) return tree[i];
         int m = (l + r) / 2;
@@ -71,17 +77,17 @@ struct SegmentTree {
 };
 
 void solve() {
-    
+	cout << __cplusplus;
 }
 
 int main() {
-    fast_io
-    #ifndef LOCAL
-    freopen((NAME + ".INP").c_str(), "r", stdin);
-    freopen((NAME + ".OUT").c_str(), "w", stdout);
-    #endif
+	fast_io
+	#ifndef LOCAL
+		freopen("INPUT.INP", "r", stdin);
+		freopen("OUTPUT.OUT", "w", stdout);
+	#endif
 
-    int t = 1;
-    // cin >> t;
-    while (t--) solve();
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
 }
