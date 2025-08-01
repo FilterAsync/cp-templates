e047d4490d3717c5e546bd334f0ac349db305771
goppamach <81472214+FilterAsync@users.noreply.github.com>
2025-08-01 14:42:32 +0700

    update grading template

--- misc/grading/template.cpp
+++ misc/grading/template.cpp
@@ -2,14 +2,6 @@
 
 using namespace std;
 
-template<class T> bool chmax(T &a, T const &b) {
-    return (a < b ? (a = b, true) : false);
-}
-
-template<class T> bool chmin(T &a, T const &b) {
-    return (a > b ? (a = b, true) : false);
-}
-
 using ll = long long;
 
 #define el "\n"
@@ -17,26 +9,22 @@ using ll = long long;
 #define pll pair<ll, ll>
 #define fi first
 #define se second
-#define FOR(i, l, r) for (ll i = l; i <= r; i++)
-#define FOD(i, l, r) for (ll i = l; i >= r; i--)
-
+#define FOR(i, l, r) for (int i = l; i <= (r); i++)
+#define FOD(i, l, r) for (int i = l; i >= (r); i--)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) (int)(x).size()
-#define by(x) [](auto const &a, auto const &b) { return a.x < b.x; }
-
 #define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
-#define LOCAL
+template<class T> bool chmax(T &a, T const &b) { return (a < b ? (a = b, true) : false); }
+
+template<class T> bool chmin(T &a, T const &b) { return (a > b ? (a = b, true) : false); }
 
 mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
 
-const int MAXN = 1E5 + 5;
-const int INF = 1E9 + 7;
-const ll MOD = 1E9 + 7, INFLL = 1E18;
-const double EPS = 1E-9;
-const string NAME = "template"; // program name
-const int NTEST = 100; // number of test cases
+constexpr int NTEST = 100; // number of test cases
+
+#define NAME ""
 
 // find random number in [l, r]
 ll Rand(ll l, ll r) {
@@ -47,18 +35,18 @@ ll Rand(ll l, ll r) {
 int main() {
     srand(time(NULL));
     FOR(_tc, 1, NTEST) {
-        ofstream inp((NAME + ".INP").c_str());
-		// generate test cases here
+        ofstream inp(NAME ".INP");
+        // generate test cases here
         inp.close();
 
-        system((NAME + ".exe").c_str());
-        system((NAME + "_sol.exe").c_str());
-
-        if (system(("fc " + NAME + ".OUT " + NAME + ".ANS").c_str()) != 0) {
-            cout << "Test " << _tc << ": WA" << el;
-			return 0;
+        // answer comparison procedure
+        system(NAME ".exe");
+        system(NAME "_sol.exe");
+        cout << "Test " << _tc << ": ";
+        if (system("fc " NAME ".OUT " NAME ".ANS") != 0) {
+            cout << "WA" << el;
+            return 0;
         }
-
-    	cout << "Test " << _tc << ": AC" << el;
+        cout << "AC" << el;
     }
 }
