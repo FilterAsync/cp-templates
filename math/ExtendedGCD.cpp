f90ca2c7cc754e21388521fee0a1c9652c040ea5
goppamach <81472214+FilterAsync@users.noreply.github.com>
2025-08-09 10:55:37 +0700

    update

--- math/ExtendedGCD.cpp
+++ math/ExtendedGCD.cpp
@@ -45,8 +45,25 @@ constexpr int INF = 1E9 + 7;
 constexpr ll MOD = 1E9 + 7, INFLL = 1E18 + 7;
 constexpr double EPS = 1E-9;
 
+int ext_gcd(int a, int b, int &x, int &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = ext_gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
 void solve() {
-    
+    int a, b;
+    cin >> a >> b;
+    int x, y;
+    cout << ext_gcd(a, b, x, y) << el;
+    cout << x << " " << y << el;
 }
 
 int main() {
